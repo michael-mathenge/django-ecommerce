7c38fa8500fff84a5d534c1e569a6dc21df72008
Michael Mathenge <mmathenge1@gmail.com>
2017-05-30 02:02:28 +0300

    static files and HTML templates

---
+++ profiles/models.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class Profile(models.Model):
+    name = models.CharField(max_length=120)
+    description = models.TextField(default='description default text')
+
+    def __unicode__(self):
+        return self.name
